7e1555328d1dd5a25cdb9f586bb2d96db506769e
katya1331 <152582001+katya1331@users.noreply.github.com>
2024-12-02 08:16:21 +0500

    Add files via upload

---
+++ GCDConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using NaturalNumsLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace GCDConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введите первое число: ");
+            int First = int.Parse(ReadLine());
+
+            WriteLine("Введите второе число: ");
+            int Second = int.Parse(ReadLine());
+
+            int Gcd = NaturalNumbers.GCD(First, Second);
+
+            WriteLine($"НОД({First}, {Second}) = {Gcd}");
+            ReadLine();
+        }
+    }
+}
